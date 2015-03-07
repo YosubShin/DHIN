d94f88fdce87659c28e9c447e98928cc0a5d95a8
Luke Leslie <lmlesli2@sagan.cs.illinois.edu>
2015-03-07 09:09:25 -0600

    fix to run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-spark-submit --class "SimpleApp" --master spark://172.17.145.160:7077 target/scala-2.10/dhin_2.10-0.1-SNAPSHOT.jar
+spark-submit --class "SimpleApp" --master spark://mustang2:7077 --deploy-mode client target/scala-2.10/dhin_2.10-0.1-SNAPSHOT.jar
